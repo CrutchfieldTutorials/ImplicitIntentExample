7826331f24fe486214cecf62d92162a8b683a366
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2017-12-29 16:50:47 -0500

    Create implicit intent exercise

--- app/src/main/java/com/example/ioutd/implicitintentexample/MainActivity.java
+++ app/src/main/java/com/example/ioutd/implicitintentexample/MainActivity.java
@@ -21,12 +21,6 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Buttons that will call the implicit intents
-    @BindView(R.id.button_call) Button buttonCall;
-    @BindView(R.id.button_map) Button buttonMap;
-    @BindView(R.id.button_webpage) Button buttonWebpage;
-    @BindView(R.id.button_email) Button buttonEmail;
-    @BindView(R.id.button_calendar) Button buttonCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,140 +28,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Use annotations to bind the views in this activity with ButterKnife
         ButterKnife.bind(this);
-
     }
 
-    /**
-     * Calls a phone number
-     * @param view
-     */
-    @OnClick(R.id.button_call)
-    public void callClicked(View view){
-        // Try changing this value
-        String numberString = "555-555-5555";
-        // The phone number to be called
-        Uri numberUri = Uri.parse("tel:" + numberString);
+
+    // TODO (6): Implement the callClicked method
+        // Create a uri containing the phone number to be called
         // Create the call intent to perform the dial action
-        Intent callIntent = new Intent(Intent.ACTION_DIAL, numberUri);
         // Start the activity if the intent can be handled
-        startActivityIfVerified(callIntent);
-    }
 
-    /**
-     * Displays a location on the map
-     * @param view
-     */
-    @OnClick(R.id.button_map)
-    public void mapClicked(View view){
-        // Try changing this value
-        String locationString = "Mount Mihara, Japan";
-        // Properly formats the string ex: 'Mount+Mihara,+Japan'
-        locationString = locationString.replace(' ', '+');
-        // The location to be displayed
-        Uri locationUri = Uri.parse("geo:0,0?q=" + locationString);
+
+    // TODO (7): Implement the mapClicked method
+        // Create a uri containing the location to be displayed
         // Create the map intent to perform the view action
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
-        // Start the activity if the intent can be handled
-        startActivityIfVerified(mapIntent);
-    }
+        // Start the activity if the map intent can be handled
 
-    /**
-     * Displays a webpage
-     * @param view
-     */
-    @OnClick(R.id.button_webpage)
-    public void webpageClicked(View view){
-        // The webpage to be displayed
-        Uri webpageUri = Uri.parse("http://www.tesla.com/models");
+
+    // TODO (8): Implement the webpageClicked method
+        // Create a uri containing the webpage to be displayed
         // Create the web intent to perform the view action
-        Intent webIntent = new Intent(Intent.ACTION_VIEW, webpageUri);
-        // Start the activity if the intent can be handled
-        startActivityIfVerified(webIntent);
-    }
+        // Start the activity if the web intent can be handled
 
-    /**
-     * Sends an email
-     * @param view
-     */
-    @OnClick(R.id.button_email)
-    public void emailClicked(View view){
-        // Try changing these three variables
-        String email = "google@example.com";
-        String subject = "Test Subject";
-        String bodyText = "Test body text";
 
-        // Create email Intent
-        Intent emailIntent = new Intent(Intent.ACTION_SEND)
-                // Set the type of data being sent
-                .setType("text/plain")
-                // Add the destination email as an extra
-                .putExtra(Intent.EXTRA_EMAIL, new String[] {email})
-                // Add the subject is an extra
-                .putExtra(Intent.EXTRA_SUBJECT, subject)
-                // Add the body text as an extra
-                .putExtra(Intent.EXTRA_TEXT, bodyText);
+    // TODO (9): Implement the emailClicked method
+        // Create 3 String variables: email, subject, and bodyText
+        // Create the email intent to perform the send action
+            // Set the type to "text/plain"
+            // Use the putExtra method to pass in the email, subject, and bodyText
+        // Display a chooser to select which app will handle the intent
 
-        // Display apps that can send emails
-        showAppChooser(emailIntent);
-    }
 
-    /**
-     * Creates a calendar event
-     * @param view
-     */
-    @OnClick(R.id.button_calendar)
-    public void calendarClicked(View view){
+    // TODO (10): Implement the emailClicked method
         // Set the start time to now
-        Calendar calendar = Calendar.getInstance(Locale.US);
-        long beginTime = calendar.getTimeInMillis();
         // Set the end time an hour from now
-        // Try changing this variable
-        // Ex: calendar.add(Calendar.MINUTE, 15);
-        calendar.add(Calendar.HOUR, 1);
-        long endTime = calendar.getTimeInMillis();
-
         // Create the calendar intent with the insert action
-        Intent calendarIntent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI)
-                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime)
-                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime)
-                .putExtra(CalendarContract.Events.TITLE, "Sky Diving")
-                .putExtra(CalendarContract.Events.EVENT_LOCATION, "The Moon");
-
         // Start the activity if the intent can be handled
-        startActivityIfVerified(calendarIntent);
-    }
 
-    /**
-     * Starts the activity if there is an app installed that can handle it. Otherwise, notified the
-     * user that it can't.
-     * @param intent the intent to verify
-     */
-    private void startActivityIfVerified(Intent intent){
+
+    // TODO (4): Create a private method called startActivityIfVerified with an Intent as the parameter
         // Get the package manager
-        PackageManager packageManager = getPackageManager();
-        // Retrieve all of the activities that can handle this intent
-        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
+        // Query the package manager for activities that can handle the intent
+        // If the returned list isn't empty, start the activity. Otherwise notify the user there's no apps
+            //using a toast message
 
-        if (activities.size() > 0) {
-            // Start the activity of the intent with action to be performed
-            startActivity(intent);
-        } else {
-            // Notify the user there is no app to receive this intent
-            Toast.makeText(this, R.string.cant_receive_intent, Toast.LENGTH_SHORT).show();
-        }
-    }
 
-    /**
-     * Displays a chooser for selecting an app to handle an intent
-     * @param intent the intent to be handled
-     */
-    private void showAppChooser(Intent intent) {
+    // TODO (5 Create a method called showAppChooser with an Intent as the parameter
         // Create a title for the chooser
-        String title = getResources().getString(R.string.chooser_title);
         // Create an intent for the chooser
-        Intent chooserIntent = Intent.createChooser(intent, title);
         // Start the activity to display the chooser
-        startActivityIfVerified(chooserIntent);
-    }
+
 
 }
